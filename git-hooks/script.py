0cf902367ea80c0e48f523d6c2cc61f418433f61
Neil <neil.dutoit@qdivision.co>
2020-12-01 15:12:22 +0200

    some progress on git hook

---
+++ git-hooks/script.py
@@ -0,0 +1,6 @@
+"""Just some function that can be tested with a unit test
+"""
+
+
+def foo(num1: int, num2: int):
+    return num1 + num2
---
+++ git-hooks/test_script.py
@@ -0,0 +1,17 @@
+import unittest
+import script
+
+class TestScript(unittest.TestCase):
+    def run(self, result=None):
+        if not result.errors:
+            super(TestScript, self).run(result)
+        else:
+            print("ERROORRRRR")
+
+
+    def test_script(self):
+        self.assertEqual(script.foo(2,3), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()

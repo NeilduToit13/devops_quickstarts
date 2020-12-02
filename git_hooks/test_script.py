86e96a42638563db4e07ee299d97fcd4002d7f6e
Neil <neil.dutoit@qdivision.co>
2020-12-02 08:55:17 +0200

    removed old test code

--- git_hooks/test_script.py
+++ git_hooks/test_script.py
@@ -2,13 +2,6 @@ import unittest
 import script
 
 class TestScript(unittest.TestCase):
-    def run(self, result=None):
-        if not result.errors:
-            super(TestScript, self).run(result)
-        else:
-            print("ERROORRRRR")
-
-
     def test_script(self):
         self.assertEqual(script.foo(2,3), 5)
 
